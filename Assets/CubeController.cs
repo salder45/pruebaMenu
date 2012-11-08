e268ab2aff581d795b2fb877c88f5144a0d4a769
Eder <edermtz.04@gmail.com>
2012-11-07 20:04:30 -0600

    funcionalidad poner en el centro en base de puntos dinamicos completa

--- Assets/CubeController.cs
+++ Assets/CubeController.cs
@@ -15,40 +15,34 @@ public class CubeController : MonoBehaviour {
 	private DepthGenerator depth;
 	private HandsGenerator hands;
 	private GestureGenerator gesture;
-	//Punto donde inicia el Escenario
-	private Point3D pEI=Point3D.ZeroPoint;
-	//punto donde terminar el escenario
-	private Point3D pO=Point3D.ZeroPoint;
-	//Coordenadas para punto inicial
+	//ultimo punto de la mano
+	private Point3D puntoInicial;
+	private Point3D puntoFinal;
+	//puntos inicial y final
 	private float xInit=15.93f;
 	private float yInit=3.85f;
 	private float zInit=-50.91f;
-	//Coordenadas Finales
 	private float xEnd=-0.009f;
 	private float yEnd=9.71f;
 	private float zEnd=-34.93f;
-	//distancias por eje
+	//distancias entre los puntos
 	private float distanciaX=0f;
 	private float distanciaY=0f;
 	private float distanciaZ=0f;
-	//signos de las coordenadas
-	private string signoXInit="*";
-	private string signoYInit="*";
-	private string signoZInit="*";
-	private string signoXEnd="*";
-	private string signoYEnd="*";
-	private string signoZEnd="*";
+	//puntos medios
+	private Point3D puntoMitad;
+	private float xMedia=0f;
+	private float yMedia=0f;
+	private float zMedia=0f;
+	
 	
 	void Start () {
 		//Debug.Log("START");
-		//Inicializar puntos
-		this.pEI=new Point3D(xInit,yInit,zInit);
-		this.pO=new Point3D(xEnd,yEnd,zEnd);
-		//inicia las distancias
-		calculaDistanciaEjes();	
-		float puntoMedioX=(distanciaX);		
-		Debug.Log(puntoMedioX);
-		Debug.Log((puntoMedioX+xInit));
+		determinaCentro();
+		
+		Debug.Log("X "+xMedia+" Y "+yMedia+" Z "+zMedia);
+		transform.position=new Vector3(xMedia,yMedia,zMedia);
+		
 		this.context=Context.CreateFromXmlFile(XML_CONFIG, out scriptNode);
 		this.depth=this.context.FindExistingNode(NodeType.Depth) as DepthGenerator;
 		if(depth==null){
@@ -102,44 +96,38 @@ public class CubeController : MonoBehaviour {
 	
 	void hands_HandDestroy(object sender, HandDestroyEventArgs e){
 		Debug.Log("Mano destroy");
-	}	
+	}
 	
-	void calculaDistanciaEjes(){
-		distanciaX=distanciaEntreDosPuntos(new Point3D(xInit,0f,0f),new Point3D(xEnd,0f,0f));
-		distanciaY=distanciaEntreDosPuntos(new Point3D(0f,yInit,0f),new Point3D(0f,yEnd,0f));
-		distanciaZ=distanciaEntreDosPuntos(new Point3D(0f,0f,zInit),new Point3D(0f,0f,zEnd));
+	
+	void inicializaPuntos(){
+		this.puntoInicial=new Point3D(xInit,yInit,zInit);
+		this.puntoFinal=new Point3D(xEnd,yEnd,zEnd);
 	}
 	
 	float distanciaEntreDosPuntos(Point3D a,Point3D b){
-		return Mathf.Sqrt(elevaCuadrado(b.X-a.X)+elevaCuadrado(b.Y-a.Y)+elevaCuadrado(b.Z-a.Z));
+		return Mathf.Sqrt(elevaCuadrado(a.X-b.X)+elevaCuadrado(a.Y-b.Y)+elevaCuadrado(a.Z-b.Z));
 	}
 	
 	float elevaCuadrado(float numero){
 		return Mathf.Pow(numero,2f);
 	}
 	
-	void determinaSignos(){
+	void calculaDistanciaEje(){
+		distanciaX=distanciaEntreDosPuntos(new Point3D(xInit,0f,0f),new Point3D(xEnd,0f,0f));
+		distanciaY=distanciaEntreDosPuntos(new Point3D(0f,yInit,0f),new Point3D(0f,yEnd,0f));
+		distanciaZ=distanciaEntreDosPuntos(new Point3D(0f,0f,zInit),new Point3D(0f,0f,zEnd));
 		
 	}
 	
-	string retornaSignoNumero(float numero){
-		string retorno="*";
-		if(isPositivo(numero)){
-			retorno=POSITIVO;
-		}else {
-			retorno=NEGATIVO;
-		}
-		
-		return retorno;
+	void calculaPuntoMedio(){
+		xMedia=xInit-(distanciaX/2);
+		yMedia=yInit+(distanciaY/2);
+		zMedia=zInit+(distanciaZ/2);
 	}
 	
-	bool isPositivo(float number){
-		bool retorno=true;
-		if(number<0){
-			retorno=false;
-		}
-		
-		return retorno;
+	void determinaCentro(){
+		inicializaPuntos();
+		calculaDistanciaEje();
+		calculaPuntoMedio();
 	}
-	
 }
