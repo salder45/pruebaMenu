c966409aacef2f3b1a309f6e33b5d380c7198be2
Eder <edermtz.04@gmail.com>
2012-11-08 18:09:20 -0600

    funcionalidad de menu casi completa excepto ejes invertidos y z esta mal

--- Assets/CubeController.cs
+++ Assets/CubeController.cs
@@ -8,8 +8,9 @@ using NITE;
 public class CubeController : MonoBehaviour {
 	private readonly string XML_CONFIG=@".//OpenNI.xml";
 	private const string WAVE="Wave";
-	private const string POSITIVO="+";
-	private const string NEGATIVO="-";
+	private const float MEDIDA_MANO_X=400f;
+	private const float MEDIDA_MANO_Y=300f;
+	private const float MEDIDA_MANO_Z=300f;
 	private Context context;
 	private ScriptNode scriptNode;
 	private DepthGenerator depth;
@@ -34,13 +35,26 @@ public class CubeController : MonoBehaviour {
 	private float xMedia=0f;
 	private float yMedia=0f;
 	private float zMedia=0f;
+	//puntos mano
+	private Point3D puntoMano;
+	private float kinectManoX=0f;
+	private float kinectManoY=0f;
+	private float kinectManoZ=0f;
+	//punto de referencia para el kinect
+	private float kinectRefX=0f;
+	private float kinectRefY=0f;
+	private float kinectRefZ=0f;
+	//distancias reales kinect
+	private float kinectDistanciaX=0f;
+	private float kinectDistanciaY=0f;
+	private float kinectDistanciaZ=0f;
 	
 	
 	void Start () {
 		//Debug.Log("START");
 		determinaCentro();
 		
-		Debug.Log("X "+xMedia+" Y "+yMedia+" Z "+zMedia);
+		//Debug.Log("X "+xMedia+" Y "+yMedia+" Z "+zMedia);
 		transform.position=new Vector3(xMedia,yMedia,zMedia);
 		
 		this.context=Context.CreateFromXmlFile(XML_CONFIG, out scriptNode);
@@ -88,10 +102,19 @@ public class CubeController : MonoBehaviour {
 	
 	void hands_HandCreate(object sender, HandCreateEventArgs e){
 		Debug.Log("Mano Creada");
+		this.puntoMano=e.Position;
+		setPuntosManoActuales(this.puntoMano);
+		iniciaPuntoReferencia(this.puntoMano);
+		//Debug.Log("X "+puntoMano.X+" Y "+puntoMano.Y+" Z "+puntoMano.Z);
+		//Debug.Log("**** X "+kinectRefX+" Y "+kinectRefY+" Z "+kinectRefZ);
+		
 	}
 	
 	void hands_HandUpdate(object sender, HandUpdateEventArgs e){
-		Debug.Log("Mano Update");
+		//Debug.Log("Mano Update");
+		setPuntosManoActuales(e.Position);
+		distanciaKinectReales();
+		calculaMovimiento();
 	}
 	
 	void hands_HandDestroy(object sender, HandDestroyEventArgs e){
@@ -113,16 +136,16 @@ public class CubeController : MonoBehaviour {
 	}
 	
 	void calculaDistanciaEje(){
-		distanciaX=distanciaEntreDosPuntos(new Point3D(xInit,0f,0f),new Point3D(xEnd,0f,0f));
-		distanciaY=distanciaEntreDosPuntos(new Point3D(0f,yInit,0f),new Point3D(0f,yEnd,0f));
-		distanciaZ=distanciaEntreDosPuntos(new Point3D(0f,0f,zInit),new Point3D(0f,0f,zEnd));
+		this.distanciaX=distanciaEntreDosPuntos(new Point3D(xInit,0f,0f),new Point3D(xEnd,0f,0f));
+		this.distanciaY=distanciaEntreDosPuntos(new Point3D(0f,yInit,0f),new Point3D(0f,yEnd,0f));
+		this.distanciaZ=distanciaEntreDosPuntos(new Point3D(0f,0f,zInit),new Point3D(0f,0f,zEnd));
 		
 	}
 	
 	void calculaPuntoMedio(){
-		xMedia=xInit-(distanciaX/2);
-		yMedia=yInit+(distanciaY/2);
-		zMedia=zInit+(distanciaZ/2);
+		this.xMedia=xInit-(distanciaX/2);
+		this.yMedia=yInit+(distanciaY/2);
+		this.zMedia=zInit+(distanciaZ/2);
 	}
 	
 	void determinaCentro(){
@@ -130,4 +153,36 @@ public class CubeController : MonoBehaviour {
 		calculaDistanciaEje();
 		calculaPuntoMedio();
 	}
+	
+	void iniciaPuntoReferencia(Point3D puntoCentral){
+		this.kinectRefX=puntoCentral.X+(MEDIDA_MANO_X/2f);
+		this.kinectRefY=puntoCentral.Y-(MEDIDA_MANO_Y/2f);
+		this.kinectRefZ=puntoCentral.Z+(MEDIDA_MANO_Z/2f);
+	}
+	
+	void setPuntosManoActuales(Point3D a){
+		this.kinectManoX=a.X;
+		this.kinectManoY=a.Y;
+		this.kinectManoZ=a.Z;
+	}
+	
+	void distanciaKinectReales(){
+		this.kinectDistanciaX=distanciaEntreDosPuntos(new Point3D(kinectRefX,0f,0f),new Point3D(kinectManoX,0f,0f));
+		this.kinectDistanciaY=distanciaEntreDosPuntos(new Point3D(0f,kinectRefY,0f),new Point3D(0f,kinectManoY,0f));
+		this.kinectDistanciaZ=distanciaEntreDosPuntos(new Point3D(0f,0f,kinectRefZ),new Point3D(0f,0f,kinectManoZ));
+	}
+	
+	void calculaMovimiento(){
+		//Debug.Log("Calcula Movimiento");
+		float xNormal=((kinectDistanciaX/10f)*100f)/(MEDIDA_MANO_X/10f);
+		float yNormal=((kinectDistanciaY/10f)*100f)/(MEDIDA_MANO_Y/10f);
+		float zNormal=((kinectDistanciaZ/10f)*100f)/(MEDIDA_MANO_Z/10f);
+		float x=(xNormal*distanciaX)/100;
+		float y=(yNormal*distanciaY)/100;
+		float z=(zNormal*distanciaZ)/100;
+		Debug.Log(xNormal);
+		
+		transform.position=new Vector3(x,y,transform.position.z);
+	}
+
 }
